57832bf4f68da9536300e579d12f64c1c06485fd
ZaharinVN <vitalikz85@yandex.ru>
2023-05-21 17:09:11 +0300

    Commit themes

--- app/src/main/java/com/example/playlistmaker/MainActivity.kt
+++ app/src/main/java/com/example/playlistmaker/MainActivity.kt
@@ -1,28 +1,30 @@
 package com.example.playlistmaker
+import android.annotation.SuppressLint
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val btn_search = findViewById<Button>(R.id.btn_search)
-        btn_search.setOnClickListener {
+        val btnSearch = findViewById<Button>(R.id.btnSearch)
+        btnSearch.setOnClickListener {
             val intent = Intent(this, SearchActivity::class.java)
             startActivity(intent)
         }
 
-        val btn_library = findViewById<Button>(R.id.btn_library)
-        btn_library.setOnClickListener {
+        val btnLibrary = findViewById<Button>(R.id.btnLibrary)
+        btnLibrary.setOnClickListener {
             val intent = Intent(this, LibraryActivity::class.java)
             startActivity(intent)
         }
 
-        val btn_settings = findViewById<Button>(R.id.btn_settings)
-        btn_settings.setOnClickListener {
+        val btnSettings = findViewById<Button>(R.id.btnSettings)
+        btnSettings.setOnClickListener {
             val intent = Intent(this, SettingsActivity::class.java)
             startActivity(intent)
         }
