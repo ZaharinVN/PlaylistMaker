ce18422356d1119fdb4054893a6d6e794724b4da
ZaharinVN <vitalikz85@yandex.ru>
2023-05-25 00:37:13 +0300

    Commit.Sprint 9.0

--- app/src/main/java/com/example/playlistmaker/SearchActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SearchActivity.kt
@@ -1,11 +1,92 @@
 package com.example.playlistmaker
 
+import android.content.Context
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.util.Log
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ImageView
 
 class SearchActivity : AppCompatActivity() {
+    private var searchQuery: String = ""
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_search)
+
+        val backButton = findViewById<Button>(R.id.btnSettingsBack)
+        backButton.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+
+        val searchEditText = findViewById<EditText>(R.id.searchEditText)
+        val clearImageView = findViewById<ImageView>(R.id.clearImageView)
+        searchEditText.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                if (s?.length ?: 0 > 0) {
+                    clearImageView.visibility = View.VISIBLE
+                } else {
+                    clearImageView.visibility = View.GONE
+                }
+                searchQuery = s.toString()
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+                // заглушка, добавить код для работы с поисковым запросом
+            }
+        })
+        clearImageView.setOnClickListener {
+            // Очищаем поисковый запрос
+            searchEditText.setText("")
+            // Скрываем клавиатуру
+            val inputMethodManager =
+                getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+            inputMethodManager.hideSoftInputFromWindow(searchEditText.windowToken, 0)
+            // Скрываем кнопку сброса
+            clearImageView.visibility = View.GONE
+        }
+        searchEditText.setOnFocusChangeListener { v, hasFocus ->
+            // Отображение клавиатуры и фокуса на поле ввода поискового запроса
+            if (hasFocus) {
+                val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+                imm.showSoftInput(v, InputMethodManager.SHOW_IMPLICIT)
+                clearImageView.visibility =
+                    if (searchEditText.text.toString().isEmpty()) View.GONE else View.VISIBLE
+            }
+            // Скрытие кнопки сброса поискового запроса и клавиатуры при потере фокуса
+            else {
+                clearImageView.visibility = View.GONE
+                val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+                imm.hideSoftInputFromWindow(window.decorView.windowToken, 0)
+            }
+        }
+
+        searchEditText.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                clearImageView.visibility = if (s.toString().isEmpty()) View.GONE else View.VISIBLE
+            }
+
+            override fun afterTextChanged(s: Editable?) {}
+        })
+        fun onSaveInstanceState(outState: Bundle) {
+            outState.putString("search_query", searchQuery)
+            super.onSaveInstanceState(outState)
+        }
+
+        fun onRestoreInstanceState(savedInstanceState: Bundle) {
+            super.onRestoreInstanceState(savedInstanceState)
+            searchQuery = savedInstanceState.getString("search_query", "")
+            val searchText = findViewById<EditText>(R.id.searchEditText)
+            searchText.setText(searchQuery)
+        }
     }
 }
