f560d1f76344ac377eef755c559d14f274257578
ZaharinVN <vitalikz85@yandex.ru>
2023-07-12 22:36:33 +0300

    Sprint 12.3

--- app/src/main/java/com/example/playlistmaker/ItunesSearchResult.kt
+++ app/src/main/java/com/example/playlistmaker/ItunesSearchResult.kt
@@ -1,13 +1,48 @@
 package com.example.playlistmaker
 
 import android.icu.util.TimeUnit
+import android.os.Parcel
+import android.os.Parcelable
 import java.text.SimpleDateFormat
 import java.util.Date
 import java.util.Locale
 
 data class ItunesSearchResult(
+    val trackId: String,
     val trackName: String,
     val artistName: String,
     val trackTimeMillis: String,
-     val artworkUrl100: String
-     )
+    val artworkUrl100: String,
+) : Parcelable {
+
+    constructor(parcel: Parcel) : this(
+        parcel.readString()!!,
+        parcel.readString()!!,
+        parcel.readString()!!,
+        parcel.readString()!!,
+        parcel.readString()!!
+    )
+
+    override fun writeToParcel(parcel: Parcel, flags: Int) {
+        parcel.writeString(trackId)
+        parcel.writeString(trackName)
+        parcel.writeString(artistName)
+        parcel.writeString(trackTimeMillis)
+        parcel.writeString(artworkUrl100)
+    }
+
+    override fun describeContents(): Int {
+        return 0
+    }
+
+    companion object CREATOR : Parcelable.Creator<ItunesSearchResult> {
+        override fun createFromParcel(parcel: Parcel): ItunesSearchResult {
+            return ItunesSearchResult(parcel)
+        }
+
+        override fun newArray(size: Int): Array<ItunesSearchResult?> {
+            return arrayOfNulls(size)
+        }
+    }
+}
+
